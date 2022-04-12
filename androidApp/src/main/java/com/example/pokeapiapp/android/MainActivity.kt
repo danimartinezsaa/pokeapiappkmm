5cc4a7242fc94113ccd42560ca44bba6865d8e9a
Dani Martínez <daniel.martinez@quobis.com>
2022-04-12 15:44:30 +0200

    add pokemon list feature to android app

--- androidApp/src/main/java/com/example/pokeapiapp/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/pokeapiapp/android/MainActivity.kt
@@ -2,21 +2,43 @@ package com.example.pokeapiapp.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.pokeapiapp.Greeting
-import android.widget.TextView
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.pokeapiapp.Pokemon
 import com.example.pokeapiapp.Pokemons
-
-fun greet(): String {
-    return Greeting().greeting()
-}
+import kotlinx.coroutines.*
+import java.lang.NullPointerException
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recyclerView: RecyclerView
+
+    private val pokemonsAdapter by lazy {
+        PokemonsAdapter()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        recyclerView = findViewById(R.id.pokemonList)
+        recyclerView.setHasFixedSize(true)
+
+        val layoutManager = LinearLayoutManager(baseContext)
+        recyclerView.layoutManager = layoutManager
+
+        recyclerView.adapter = pokemonsAdapter
+
+        GlobalScope.async {
+            withContext(Dispatchers.Main) {
+                try {
+                    val pokemons = Pokemons().getPokemons(20).map { it.name }.toTypedArray()
+                    pokemonsAdapter.setData(pokemons)
+                } catch (error: Error) {
+                    Log.d("Error: ", "$error")
+                }
+            }
+        }
     }
 }
---
+++ androidApp/src/main/java/com/example/pokeapiapp/android/PokemonsAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.pokeapiapp.android
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pokeapiapp.Pokemon
+
+class PokemonsAdapter(): RecyclerView.Adapter<PokemonsAdapter.ViewHolder>() {
+
+    private var dataSet: Array<String> = emptyArray()
+
+    class ViewHolder(var textView: TextView) : RecyclerView.ViewHolder(textView)
+
+    @SuppressLint("ResourceType")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context)
+            .inflate(R.layout.pokemon_list_cell, parent, false) as TextView
+
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = dataSet[position]
+    }
+
+    override fun getItemCount() = dataSet.size
+
+    fun setData(newData: Array<String>) {
+        dataSet = newData
+        notifyDataSetChanged()
+    }
+}
